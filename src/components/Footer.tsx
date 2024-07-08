2f32564c9f537bd1f0a6c9492dedccaba3e9f380
cshorg <colegrohs@outlook.com>
2024-07-07 21:45:05 -0700

    refactoring structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="sticky bottom-0 left-0 flex items-start justify-between w-full">
-      <p className="text-neutral-50 text-[14px] ">Cole Grohs</p>
+    <footer className="flex items-start justify-between w-full mb-10">
+      <p className="text-neutral-500 text-[12px]">Colegrohs@outlook.com</p>
       <a
         href="https://github.com/cshorg/portfolio"
-        className="text-neutral-50 text-[14px] hover:underline cursor-pointer"
+        className="text-neutral-50 text-[12px] hover:underline cursor-pointer"
       >
         Source
       </a>
