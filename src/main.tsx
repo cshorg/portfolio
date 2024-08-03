90644677455beb490ede03f8744b6a25fcc80502
cshorg <colegrohs@outlook.com>
2024-08-02 20:30:14 -0700

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { ThemeProvider } from "./components/theme-provider.tsx"
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </ThemeProvider>
+  </React.StrictMode>,
 )
