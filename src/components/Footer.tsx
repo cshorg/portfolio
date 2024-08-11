62b60b5ed8caac992afb0d09a3b6648854775832
cshorg <colegrohs@outlook.com>
2024-08-10 22:20:55 -0700

    footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
     <footer
       className={`${className} absolute bottom-0 flex items-center justify-between w-full max-w-screen-lg mx-4 my-4 text-xs md:mt-6 md:mx-10 lg:ml-20 2xl:mx-36`}
     >
-      <p className="dark:text-zinc-500 ">
+      <p className="dark:text-zinc-500 text-zinc-950">
         Created by{" "}
         <a className="cursor-pointer dark:text-white hover:underline">
           (@cshorgx)
