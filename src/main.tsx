9a66ccf8e44a7dbdd0de1073dc55a9a7c0d2e35e
cshorg <colegrohs@outlook.com>
2024-08-13 16:50:02 -0700

    vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css"
 import ThemeToggle from "./components/ThemeToggle.tsx"
 import { BrowserRouter } from "react-router-dom"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ThemeToggle>
       <Analytics />
+      <SpeedInsights />
       <App />
     </ThemeToggle>
   </BrowserRouter>
