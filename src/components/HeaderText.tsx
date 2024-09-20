f576f4a07ca8520d79dbbc91db794ed4f1e720a3
cshorg <colegrohs@outlook.com>
2024-09-20 00:12:30 -0700

    mobile text size change

--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -1,6 +1,6 @@
 const HeaderText = () => {
   return (
-    <h1 className="z-50 max-w-screen-lg text-3xl font-bold leading-snug tracking-tight lg:leading-snug lg:text-5xl text-zinc-950 dark:text-zinc-50">
+    <h1 className="z-50 max-w-screen-lg text-[26px] font-bold leading-snug tracking-tight lg:leading-snug lg:text-5xl text-zinc-950 dark:text-zinc-50">
       Hello my name is
       <span className="z-50 ml-2 bg-zinc-950 text-zinc-50 dark:bg-zinc-50 dark:text-zinc-950">
         Cole Grohs
