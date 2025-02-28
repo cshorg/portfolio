6e0da8cac59fca9fa94f910fb8d00f7ac3830083
cshorg <colegrohs@outlook.com>
2025-02-28 14:00:26 -0800

    Update projects and card color variant

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import Card from "../components/Card"
-import projects from "../data/projects.json"
-import Draggable from "react-draggable"
+import Card from '../components/Card'
+import projects from '../data/projects.json'
+import Draggable from 'react-draggable'
 
 const getRandomPosition = (maxX: number, maxY: number) => {
   return {
@@ -14,22 +14,22 @@ const Home = () => {
   const maxY = 200
 
   return (
-    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {projects.map((project, index) => {
         const { x, y } =
-          typeof window !== "undefined" && window.innerWidth >= 1200
+          typeof window !== 'undefined' && window.innerWidth >= 1200
             ? getRandomPosition(maxX, maxY)
             : { x: 0, y: 0 }
 
         return (
-          <Draggable key={index} bounds=".layout" defaultPosition={{ x, y }}>
+          <Draggable key={index} bounds='.layout' defaultPosition={{ x, y }}>
             <div>
               <Card
                 title={project.title}
                 description={project.description}
                 route={project.route}
                 url={project.url}
-                github={project.github}
+                github={project.github ?? ''}
                 color={project.color}
               />
             </div>
